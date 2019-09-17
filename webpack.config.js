5769f42d4904ea09eaa19b463bcaeb301ac2bbf1
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2019-09-17 18:46:07 +0900

    Update html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'production',
@@ -6,5 +7,22 @@ module.exports = {
     output: {
         path: path.join(__dirname,'dist'),
         filename: 'app.bundle.js'
-    }
+    },
+    //バンドルされる前にbabelを通す。
+    module:{
+        rules: [
+            {
+                test: /\.js$/,
+                //babel-loaderはwebpackとbabelをつなぐ、初期設定としてpreset-envを使う。(optionなので無くても大丈夫)
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/preset-env','@babel/preset-react']
+                }
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })]
 }
