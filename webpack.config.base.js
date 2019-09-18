25b49ccd51d9dab97c24db92f097946fd149bf8e
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2019-09-18 13:39:41 +0900

    Update import CSS

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -18,7 +18,12 @@ module.exports = {
                 options: {
                     presets: ['@babel/preset-env','@babel/preset-react'],
                     plugins: ['@babel/plugin-proposal-class-properties']
-                }
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader'],
+                exclude: /node_modules/
             }
         ]
     },
