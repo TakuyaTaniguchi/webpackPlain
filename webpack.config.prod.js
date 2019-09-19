3a48cea99bbac9aec67aadbb93f23f527ded86b7
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2019-09-19 15:34:09 +0900

    Update Analyze

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,12 @@
 const marge = require('webpack-merge')
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 const baseConfig = require('./webpack.config.base')
 
 module.exports = marge(baseConfig,{
     mode: 'production',
+    plugins: [new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false,
+        reportFilename: 'bundle_sizes.html'
+    })]
 })
