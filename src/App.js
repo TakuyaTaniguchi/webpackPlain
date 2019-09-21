a238f1b41f1f9b4719649d7af9ac81fff62c7c10
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2019-09-21 13:17:38 +0900

    Update コンポーネントの非同期ロード

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {hot} from 'react-hot-loader'
+// import Warning from './Warning'
+const Warning = React.lazy(() => import('./Warning'))
 class App extends React.Component {
     state = {
         count: 0
@@ -9,6 +11,11 @@ class App extends React.Component {
             <div>
                 <h1>Hello worldavbt</h1>
                 <h2 className={ this.state.count > 10 ? 'warning': null}>count: {this.state.count}</h2>
+                { this.state.count > 10 ? 
+                    <React.Suspense fallback={null}>
+                        <Warning/>
+                    </React.Suspense>
+                    : null}
                 <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
                 <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
             </div>
